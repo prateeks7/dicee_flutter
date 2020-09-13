005bb8fc3e6099986378c54f329b961e96f711c8
Prateek Sharma <62699900+prateeks7@users.noreply.github.com>
2020-09-14 03:33:40 +0530

    Dice Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'dart:math';
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.blue[900],
         appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          title: Center(child: Text('Dice')),
+          backgroundColor: Colors.indigo[900],
         ),
         body: DicePage(),
       ),
@@ -15,9 +16,60 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+  int sum = 2;
+  void change()
+  {
+    leftDiceNumber = Random().nextInt(6)+1;
+    rightDiceNumber = Random().nextInt(6)+1;
+     sum = leftDiceNumber+rightDiceNumber;
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget> [
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: FlatButton(
+                  onPressed: (){
+                    setState(() {
+                      change();
+                    });
+                  },
+                  child: Image.asset('images/dice$leftDiceNumber.png'),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  onPressed: (){
+                    setState(() {
+                      change();
+                    });
+                  },
+                  child: Image.asset('images/dice$rightDiceNumber.png'),
+                ),
+              ),
+            ],
+          ),
+          Text(
+            "$sum",
+            style: TextStyle(
+              fontSize: 100,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
